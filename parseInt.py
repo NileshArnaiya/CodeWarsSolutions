d94e70f5975afc3bba7738a6f58faf3aac95f0dc
madmumbai <nilesh@begenuin.com>
2022-01-05 23:08:11 +0530

    yes

---
+++ parseInt.py
@@ -0,0 +1,8 @@
+#In this kata we want to convert a string into an integer. The strings simply represent the numbers in words.
+
+def parse_int(string):
+    list1 = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+    list2 = [0,1,2,3,4,5,6,7,8,9,10]
+    
+    return 
+
